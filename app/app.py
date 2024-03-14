41cacd8ad9467173d4f597643694aec4727f71d3
Carlos <carlos@gmail.com>
2024-03-14 13:42:00 +0100

    Pasamos a la version 1.0.1v

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Hola mundo desde Flask que tal</h1>"
+
 if __name__ == "__main__":
-    app.run()
-    
+    app.run(debug = True, port=5000)
