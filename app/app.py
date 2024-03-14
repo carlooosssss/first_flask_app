3083526980c92bc975c5db108304b0a63813c3ef
Carlos <carlos@gmail.com>
2024-03-14 14:18:07 +0100

    Pasamos a la version 1.0.3

--- app/app.py
+++ app/app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 def index():
     #return "<h1>Hola mundo desde Flask que tal</h1>"
 
-    return render_template("index.html")
+    data = {
+        "Titulo": "Index",
+        "bienvenida": "Saludos!!!"
+    }
+
+    return render_template("index.html",data=data)
 
 if __name__ == "__main__":
     app.run(debug = True, port=5000)
